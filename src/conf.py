0670f7514578d8e618eea97ce5bf2c75b519e1a6
stk <stefan@shutterworks.org>
2020-07-13 23:13:42 +0200

    added blockdiag

--- src/conf.py
+++ src/conf.py
@@ -41,6 +41,7 @@ release = u'0.1'
 extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.githubpages',
+    'sphinxcontrib.blockdiag',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
